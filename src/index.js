fc4d9722cdf9884c41691b25e0cdf86f521f3219
Marlon Torres Lozano <developer.marlon.torres@gmail.com>
2021-06-16 13:41:38 -0500

    update info chat

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ io.on('connection', (socket) => {
     const sock = socket.adapter.rooms[room] ? Object.keys(socket.adapter.rooms[room].sockets) : []
     for(let i = 0; i < sock.length; i++){
       let user = io.in(room).sockets[sock[i]]
-      if(users.filter((item) => item.name === user.name && item.type_user === user.type_user).length === 0) users.push({name: user.name, type_user: user.type_user, id: user.id})
+      if(users.filter((item) => item.name === user.name && item.type_user === user.type_user && item.email === user.email).length === 0) users.push({name: user.name, type_user: user.type_user, id: user.id, email: user.email})
     }
     return users
   }
@@ -40,6 +40,7 @@ io.on('connection', (socket) => {
     await socket.leave(socket.room)
     await socket.join(data.room)
     socket.room = data.room
+    socket.email = data.email
     socket.name = data.name
     socket.type_user = data.type_user
     socket.broadcast.to(data.room).emit('user connected', {name: socket.name, date: moment().locale('es').format('LLL'), type_user: socket.type_user, id: socket.id})
@@ -47,7 +48,7 @@ io.on('connection', (socket) => {
   })
 
   socket.on('send message', async (message) => {
-    const messageComplete = {id: socket.id, message, date: moment().locale('es').format('LLL'), type_user: socket.type_user, name: socket.name, room: socket.room }
+    const messageComplete = {id: socket.id, message, date: moment().locale('es').format('LLL'), type_user: socket.type_user, email: socket.email, name: socket.name, room: socket.room }
     socket.broadcast.to(socket.room).emit('get message', messageComplete)
     const db = await connect()
     await db.collection('messages').insertOne(messageComplete)
